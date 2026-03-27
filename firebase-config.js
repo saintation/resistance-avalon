5098374e0e847cd3fa642147541816145721617e
singwith <156996150+saintation@users.noreply.github.com>
2026-03-27 15:19:31 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,6 +3,7 @@
 // 1. 파이어베이스 초기화 모듈 로드
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
 // 2. 내 프로젝트 고유 설정값 (이곳에만 값을 정확히 채워 넣으시면 됩니다)
 const firebaseConfig = {
@@ -21,3 +22,6 @@ const app = initializeApp(firebaseConfig);
 
 // 4. 외부 파일(index.html 등)에서 이 db 객체를 가져다 쓸 수 있도록 내보내기(export)
 export const db = getDatabase(app);
+
+// [추가] auth 객체 내보내기
+export const auth = getAuth(app);
